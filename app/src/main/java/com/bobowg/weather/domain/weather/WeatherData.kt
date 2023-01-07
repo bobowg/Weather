2051c9c7d227eeaa24baf04e6a6a060f8e0e3982
bobo <bobowg@163.com>
2023-01-07 23:01:01 +0800

    添加天气数据结构及API

--- app/src/main/java/com/bobowg/weather/domain/weather/WeatherData.kt
+++ app/src/main/java/com/bobowg/weather/domain/weather/WeatherData.kt
@@ -1,5 +1,12 @@
 package com.bobowg.weather.domain.weather
 
-class WeatherData {
+import java.time.LocalDateTime
 
-}
+data class WeatherData(
+    val time: LocalDateTime,
+    val temperatureCelsius: Double,
+    val pressure: Double,
+    val windSpeed: Double,
+    val humidity: Double,
+    val weatherType: WeatherType
+)
