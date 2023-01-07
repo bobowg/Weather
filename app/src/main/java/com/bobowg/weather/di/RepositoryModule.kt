3dedb7543b5dcdf2d64f81686ccccf2c48dfb121
bobo <bobowg@163.com>
2023-01-08 01:40:59 +0800

    添加匕首模块。

---
+++ app/src/main/java/com/bobowg/weather/di/RepositoryModule.kt
@@ -0,0 +1,17 @@
+package com.bobowg.weather.di
+
+import com.bobowg.weather.data.repository.WeatherRepositoryImpl
+import com.bobowg.weather.domain.repository.WeatherRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+    @Binds
+    @Singleton
+    abstract fun bindWeatherRepository(weatherRepositoryImpl: WeatherRepositoryImpl): WeatherRepository
+}
